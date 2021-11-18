d4b304ef270b102d7317d4c59d95145028f09fd6
PuneethKumarA <apuneethk@gmail.com>
2021-11-17 22:10:31 -0800

    getting the data only once by passing [ ] empty array to useEffect hook

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -1,20 +1,26 @@
-import React, {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function DataFetching() {
-    const [posts,setPosts] = useState([])
+    const [posts, setPosts] = useState([])
 
+    /**
+    * ///useEffect is about the lifecycle methods of a class component --
+    *  if you want to use lifecycle methods of a class component in a stateless functional component --then we use useEffect hook
+    */
     useEffect(
+        //the arrow function below will get executed after every render
         () => {
             axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then( res => {
-                console.log(res)
-                setPosts(res.data)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        }
+                .then(res => {
+                    console.log(res)
+                    setPosts(res.data)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        },
+        [] //run this useEffect hook only once ie after component did mount
     )
 
 
@@ -23,7 +29,7 @@ function DataFetching() {
         <div>
             <ul>{
                 posts.map(
-                    post => <li key = {post.id}>{post.title}</li>
+                    post => <li key={post.id}>{post.title}</li>
                 )
             }</ul>
         </div>
